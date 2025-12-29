77a82c734071e959974c3a5432124dfaa9e525c9
Jake Pichugin <jakepichugin@gmail.com>
2025-12-29 14:52:08 -0800

    Added wheels, improved turning logic

--- car.py
+++ car.py
@@ -7,9 +7,13 @@ import pygame
 import main
 pygame.init()
 
-STRAIGHT = 40
-LEFT = 0
-RIGHT = 80
+STRAIGHT = 0
+LEFT = 40
+RIGHT = -40
+
+
+
+
 class Car:
     def __init__(self):
         self.width = 50
@@ -21,47 +25,69 @@ class Car:
         self.xVel = 0.0
         self.yVel = 0.0
         self.turnSpeed = 0.0
-        self.turnDirection = STRAIGHT
+        self.turn_direction = STRAIGHT
 
         self.temp_car_surface = pygame.Surface((self.width, self.height), pygame.SRCALPHA)
         self.car_surface = self.temp_car_surface
         self.car_surface.fill((0, 255, 0))
         self.car_rect = self.car_surface.get_rect()
 
+        #left wheel then right wheel, then left back, then right back
+        self.wheels = [Wheel(self.xPos, self.yPos, True, True),
+                       Wheel(self.xPos, self.yPos, True, False),
+                       Wheel(self.xPos, self.yPos, False, True),
+                       Wheel(self.xPos, self.yPos, False, False)]
+
+
+
     def move(self):
+        #inputs from keyboard
         keys = pygame.key.get_pressed()
+
+        #input detection
+        if keys[pygame.K_LEFT]:
+            self.turn_direction = LEFT
+        elif keys[pygame.K_RIGHT]:
+            self.turn_direction = RIGHT
+        else:
+            self.turn_direction = STRAIGHT
+
         if self.speed != 0:
             if keys[pygame.K_LEFT]:
-                self.turnDirection = LEFT
+                #turning speed depends on speed
                 self.angle += (0.8 * self.speed) / (1 + (abs(1.2 * self.speed) * 0.2))
             if keys[pygame.K_RIGHT]:
-                self.turnDirection = RIGHT
+                # turning speed depends on speed
                 self.angle -= (0.8 * self.speed) / (1 + (abs(1.2 * self.speed) * 0.2))
-        else:
-            self.turnDirection = STRAIGHT
 
+
+        #gas and brake keys
         if keys[pygame.K_UP]:
             if self.speed < 10:
-                self.speed += 0.5
+                self.speed += 0.2
         if keys[pygame.K_DOWN]:
-            if self.speed > -10:
-                self.speed -= 0.5
+            if self.speed > -5:
+                self.speed -= 0.2
 
         # friction:
         if self.speed > 0:
             self.speed -= 0.1
         elif self.speed < 0:
             self.speed += 0.1
-
         if self.speed < 0.1 and self.speed > -0.1:
             self.speed = 0
 
+        # turing wheels:
+
+
 
     def transform(self):
-        if self.speed != 0 and self.turnDirection != STRAIGHT:
+        if self.speed != 0:
             self.car_surface = pygame.transform.rotate(self.temp_car_surface, self.angle)
 
+
         self.car_rect = self.car_surface.get_rect()
+
 
         radian = math.radians(self.angle + 90)
         self.xVel = self.speed * math.cos(radian)
@@ -70,9 +96,41 @@ class Car:
         self.xPos += self.xVel
         self.yPos -= self.yVel
 
-        self.car_rect.center = (self.xPos, self.yPos)
+        pivot = pygame.math.Vector2(self.xPos, self.yPos)
+        offset = pygame.math.Vector2(0, -self.height / 2)
+        rotated_offset = offset.rotate(-self.angle)
+        self.car_rect.center = (int((pivot + rotated_offset).x), int((pivot + rotated_offset).y))
 
         self.car_rect.clamp_ip(main.screen.get_rect())
 
         print(f"Angle: {self.angle} , Speed: {self.speed}, xVel: {self.xVel}, yVel: {self.yVel}")
 
+class Wheel:
+    def __init__(self, xpos, ypos, frontWheel, leftWheel):
+        self.width = 10
+        self.height = 20
+        self.xPos = xpos
+        self.yPos = ypos
+        self.angle = 0
+        self.is_front_wheel = frontWheel
+        self.is_left_wheel = leftWheel
+        self.temp_wheel_surface = pygame.Surface((self.width, self.height), pygame.SRCALPHA)
+        self.wheel_surface = self.temp_wheel_surface
+        self.wheel_surface.fill((100, 100, 100))
+        self.wheel_rect = self.wheel_surface.get_rect()
+
+    def turn(self, car):
+
+        if self.is_front_wheel:
+            self.wheel_surface = pygame.transform.rotate(self.temp_wheel_surface, car.angle + car.turn_direction)
+        if not self.is_front_wheel:
+            self.wheel_surface = pygame.transform.rotate(self.temp_wheel_surface, car.angle)
+        self.wheel_rect = self.wheel_surface.get_rect()
+
+        side_dist = -25 if self.is_left_wheel else 25
+        front_dist = -car.height + self.height if self.is_front_wheel else -self.height
+
+        wheel_offset = pygame.math.Vector2(side_dist, front_dist)
+        rotated_wheel_pos = wheel_offset.rotate(-car.angle)
+
+        self.wheel_rect.center = (int((car.xPos + rotated_wheel_pos.x)), int((car.yPos + rotated_wheel_pos.y)))
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import torch.optim as optim # manage gradient descendent and back propagation st
 import numpy as np
 import pygame
 from car import Car
+from car import Wheel
 
 player = Car()
 
@@ -12,8 +13,14 @@ screen = pygame.display.set_mode((1800, 1200))
 clock = pygame.time.Clock()
 def draw():
     screen.fill((0, 0, 0))
+
+    for wheel in player.wheels:
+        screen.blit(wheel.wheel_surface, wheel.wheel_rect)
     screen.blit(player.car_surface, player.car_rect)
     pygame.display.flip()
+
+
+
 
 running = True
 while running:
@@ -23,7 +30,8 @@ while running:
 
     player.move()
     player.transform()
-
+    for wheel in player.wheels:
+        wheel.turn(player)
     draw()
     screen.fill((0,0,0))
     clock.tick(60)
